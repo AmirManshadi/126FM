8feba9278224dc279720680705bd19e7b87eb47e
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-11-28 12:05:51 +0330

    create footer for radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import {
   AnimatedText,
-  ChannelInfo,
-  ChannelNavigation,
   Container,
+  RadioFooter,
   RadioSpeaker,
 } from "@/components";
 import { ChannelProvider } from "@/contexts/channel";
@@ -54,15 +53,17 @@ const App = () => {
     <ChannelProvider channels={CHANNELS}>
       <Container>
         <RadioSpeaker />
+
         {/* <Waveform /> */}
+
         {/* ChannelContent */}
+
         <AnimatedText
           text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi quaerat perspiciatis aut sapiente fuga. Sapiente sit dolor error ipsa nemo earum eos blanditiis dolorem tempore explicabo pariatur, velit assumenda? Repellendus labore quasi assumenda magni, rem repellat. Totam, distinctio quaerat rem eaque impedit recusandae! Exercitationem asperiores magnam sed blanditiis, incidunt qui velit a iste sint eos? Cupiditate dolores similique fuga? Unde dolorum sed tempore laudantium at error minus cupiditate voluptas quod eius quis temporibus adipisci minima ad ratione rem, atque sit? Voluptatum, odit quam. Soluta molestiae earum temporibus deleniti, nobis accusamus, error illo in vero suscipit nihil unde numquam voluptatem nisi."
           onComplete={() => setCounter(counter + 1)}
         />
 
-        <ChannelInfo />
-        <ChannelNavigation />
+        <RadioFooter />
       </Container>
     </ChannelProvider>
   );
