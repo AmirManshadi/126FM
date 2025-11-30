e0f0383f111cdc481d6743accc633ab9ab41df23
AmirManshadi <amirmanshadi.code@gmail.com>
2025-11-30 20:47:49 +0330

    feat: add content to channels with sample data for improved context

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,23 @@ const CHANNELS: Channel[] = [
     currentProgram: "",
     frequency: "98.5",
     type: ChannelType.Music,
+    content: [
+      {
+        id: "1",
+        author: "person",
+        text: "Lorem ipsum  inventore eaque necessitatibus pariatur iusto, animi molestiae eius labore omnis quasi nulla! Cum animi fugiat, dolores fuga id nemo porro obcaecati consectetur quidem assumenda deserunt eius quo blanditiis accusantium!",
+      },
+      {
+        id: "2",
+        author: "another person",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem ullam ratione ducimus veritatis neque, quae quo, cum perferendis temporibus dolore! Accusantium perferendis ipsa repellat debitis ad itaque dotibus pariatur iusto, animi molestiae eius labore omnis quasi nulla! Cum animi fugiat, dolores fuga id nemo porro obcaecati consectetur quidem assumenda deserunt eius quo blanditiis accusantium!",
+      },
+      {
+        id: "3",
+        author: "third person",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem ullam ratione ducimus veritatis neque, accusantium!",
+      },
+    ],
   },
   {
     id: "2",
@@ -23,27 +40,18 @@ const CHANNELS: Channel[] = [
     currentProgram: "",
     frequency: "99.0",
     type: ChannelType.Music,
-  },
-  {
-    id: "3",
-    name: "Channel 3",
-    currentProgram: "",
-    frequency: "101.3",
-    type: ChannelType.Music,
-  },
-  {
-    id: "4",
-    name: "Channel 4",
-    currentProgram: "",
-    frequency: "102.5",
-    type: ChannelType.Music,
-  },
-  {
-    id: "5",
-    name: "Channel 5",
-    currentProgram: "",
-    frequency: "103.7",
-    type: ChannelType.Music,
+    content: [
+      {
+        id: "1",
+        author: "person",
+        text: "Lorem ipsum dolor sit temporibus dolore! Accusantium perferendis ipsa repellat debitis ad itaque dolorem inventore eaque necessitatibus pariatur iusto, animi molestiae eius labore omnis quasi nulla! Cum animi fugiat, dolores fuga id nemo porro obcaecati consectetur quidem assumenda deserunt eius quo blanditiis accusantium!",
+      },
+      {
+        id: "2",
+        author: "another person",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem ullam ratione ducimus veritatis neque, quae eos dolorem e eaque necessitatibus pariatur iusto, animi molestiae eius labore omnis quasi nulla! Cum animi fugiat, dolores fuga id nemo porro obcaecati consectetur quidem assumenda deserunt eius quo blanditiis accusantium!",
+      },
+    ],
   },
 ];
 
@@ -54,13 +62,8 @@ const App = () => {
     <ChannelProvider channels={CHANNELS}>
       <Container>
         <RadioSpeaker />
-
         <Waveform />
-
-        {/* ChannelContent */}
-
         <Pages />
-
         <RadioFooter />
       </Container>
     </ChannelProvider>
